ddcade9a3c5168cf6c9580da04190ef0e32f5e51
Richard Ryan Marroquin <48453044+RichRMarroq@users.noreply.github.com>
2025-04-23 22:16:50 -0700

    Can upload edfinancial csv and splits per loan.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ryan's Student Loan Tracker ApP
+# Ryan's Student Loan Tracker App
 
 This application is meant to help new and exisiting students figure out what they are getting themselves into with Student Loans. Insert your Loan Details and your anticipated monthly payment to get a visual of how long it will take to pay off. 
 
@@ -6,6 +6,6 @@ This application is meant to help new and exisiting students figure out what the
 
 Tech Used: Python, Streamlit, Pandas, Numpy
 
-This whole project is based on Tech With Tim's YouTube video "How To Automate Your Finances with Python - Full Tutorial (Pandas, Sreamlit, Plotly, & More)". The original main.py located in archive/main.py will reflect a very similar file to what is shown in the video. Run it to see my take on his original idea.
+This whole project is based on Tech With Tim's YouTube video "[How To Automate Your Finances with Python - Full Tutorial (Pandas, Sreamlit, Plotly, & More)](https://youtu.be/wqBlmAWqa6A?si=kShSN9sU3wSzgw4N)". The original main.py located in archive/main.py will reflect a very similar file to what is shown in the video. Run it to see my take on his original idea.
 
 # Lessons Learned
--- main.py
+++ main.py
@@ -5,148 +5,50 @@ import json
 import os
 import time
 
+
 #to run this script use the following:
 #python -m streamlit run main.py
 
-st.set_page_config(page_title="Simple Finance App", page_icon= "💵", layout="wide")
+st.set_page_config(page_title="Student Loan Tracker", page_icon= "💵", layout="wide")
 
-category_file = "categories.json"
-
-if "categories" not in st.session_state:
-    #If state is not used, everytime the script is reran, the values are reset.
-    st.session_state.categories = {
-        "Uncategorized": []
-    }
-
-#Pull existing categories and their definitions from saved file.
-if os.path.exists(category_file):
-    with open(category_file, "r") as f:
-        st.session_state.categories = json.load(f)
-
-def save_categories():
-    with open(category_file, "w") as f:
-        json.dump(st.session_state.categories, f)
-
-def categorize_transactions(df):
-    df["Category"] = "Uncategorized"
-
-    for category, keywords in st.session_state.categories.items():
-        if category == "Uncategorized" or not keywords:
-            continue
-
-        lowered_keywords = [keyword.lower().strip() for keyword in keywords]
-
-        for idx, row in df.iterrows():
-            details = row["Description"].lower().strip()
-            if details in lowered_keywords:
-                df.at[idx, "Category"] = category
-    return df
+#TODO: Upload and store Loan Details {Original Balance, Current Balance, Interest Rate (Decimal)}
+#TODO: Calculate monthly interest growth for a student loan using the Simple Interest Formula
+#TODO: Plot out what payment life would look like only making the monthly payment.
 
 def load_transactions(file):
-    try:
-        df = pd.read_csv(file)
-        df.colums = [col.strip() for col in df.columns]
-        #Not needed as my csv already provides float value Amounts.
-        #df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
-        df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
+    df = pd.read_csv(file)
+    #EdFinancial has a trailing column, need to check for it and remove
+    if "Unnamed" in df.columns[-1]:
+        df = df.iloc[: , :-1]
 
-        #st.write(df)
-        return categorize_transactions(df)
-    except Exception as e:
-        st.error(f"Error processing file: {str(e)}")
-        return None
-
-def add_keyword_to_category(category, keyword):
-    keyword = keyword.strip()
-    if keyword and keyword not in st.session_state.categories[category]:
-        st.session_state.categories[category].append(keyword)
-        save_categories()
-        return True
-    
-    return False
+    #st.write(df)
+    return df
 
 def main():
-    st.title("Macaroni Finance Dashboard")
+    st.title("Student Loan Tracker")
 
-    uploaded_file = st.file_uploader("Upload your transaction CSV file", type=["csv"])
+    uploaded_file = st.file_uploader("Upload your Account History CSV file", type=["csv"])
 
     if uploaded_file is not None:
         df = load_transactions(uploaded_file)
 
         if df is not None:
-            spending_df = df[df["Amount"] < 0.0]
-            income_df = df[df["Amount"] > 0.0] 
+            #Get Unique loan names
+            loans = list(set(df['LoanName']))
 
-            st.session_state.spending_df = spending_df.copy()
+            #Create a dataframe for each loan
+            df_loan_dict = {name: df.loc[df['LoanName'] == name] for name in loans}
 
-            tab1, tab2 = st.tabs(["Spending", "Income"])
+            #TODO: Create tabs dynamically, for now assume 4
+            tab1, tab2, tab3, tab4 = st.tabs(loans)
             with tab1:
-                new_category = st.text_input("New Category Name")
-                add_button = st.button("Add Category")
-
-                if add_button and new_category:
-                    if new_category not in st.session_state.categories:
-                        st.session_state.categories[new_category] = []
-                        save_categories()
-                        st.success(f"Added a new category: {new_category}")
-                        time.sleep(4)
-                        st.rerun()
-                    else:
-                        time.sleep(1)
-                        st.warning(f"Category {new_category} already exists")
-                        st.rerun()
-
-                st.subheader("Your Spending")
-                edited_df = st.data_editor(
-                    st.session_state.spending_df[["Date", "Description", "Amount", "Category"]],
-                    column_config={
-                        "Date": st.column_config.DateColumn("Date", format="DD/MM/YYYY"),
-                        "Amount": st.column_config.NumberColumn("Amount", format="%.2f USD"),
-                        "Category": st.column_config.SelectboxColumn(
-                            "Category",
-                            options=list(st.session_state.categories.keys())
-                        )
-                    },
-                    hide_index=True,
-                    use_container_width=True,
-                    key="category_editor"
-                )
-
-                save_button = st.button("Apply Changes", type="primary")
-                if save_button:
-                    for idx, row in edited_df.iterrows():
-                        new_category = row["Category"]
-                        if row["Category"] == st.session_state.spending_df.at[idx, "Category"]:
-                            continue
-
-                        details = row["Description"]
-                        st.session_state.spending_df.at[idx, "Category"] = new_category
-                        add_keyword_to_category(new_category, details)
-                st.subheader('Expense Summary')
-                category_totals = st.session_state.spending_df.groupby("Category")["Amount"].sum().reset_index()
-
-                category_totals = category_totals.sort_values("Amount", ascending=False)
-
-                st.dataframe(
-                    category_totals,
-                    column_config={
-                        "Amount": st.column_config.NumberColumn("Amount", format="%.2f USD")
-                    },
-                    hide_index=True,
-                    use_container_width=True
-                )
-            
-
-                fig = px.pie(
-                    category_totals,
-                    values="Amount",
-                    names="Category",
-                    title="Expenses by Category"
-                )
-
-                st.plotly_chart(fig, use_container_width=True)
+                    st.write(df_loan_dict[loans[0]])
             with tab2:
-                st.write(income_df)
+                    st.write(df_loan_dict[loans[1]])
+            with tab3:
+                    st.write(df_loan_dict[loans[2]])
+            with tab4:
+                    st.write(df_loan_dict[loans[3]])
 
 
 main()
